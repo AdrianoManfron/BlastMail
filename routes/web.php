525a1f2d3a456aa79d99e149805eff76e384aa73
Adriano Manfron <adriano.manfron@gmail.com>
2025-01-17 12:42:48 -0300

    Criação inicial de uma lista de email

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmailListController;
 
 Route::view('/', 'welcome');
 
@@ -11,6 +12,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/email-list', [EmailListController::class, 'index'])->name('email-list.index');
+    Route::get('/email-list/create', [EmailListController::class, 'create'])->name('email-list.create');
+    Route::post('/email-list/store', [EmailListController::class, 'store'])->name('email-list.store');
 });
 
 require __DIR__ . '/auth.php';
